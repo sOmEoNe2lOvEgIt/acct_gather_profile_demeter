8830b85253110c1922889320da533aab16eed54b
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-08-05 16:51:30 +0200

    Renamed plugin for slurm conf.

--- src/demeter.c
+++ src/demeter.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 
 const char plugin_name[] = "The godess of harvesting data.";
-const char plugin_type[] = "prep/enoughnodes";
+const char plugin_type[] = "prep/demeter";
 const uint32_t plugin_version = SLURM_VERSION_NUMBER;
 
 int init (void)
@@ -19,6 +19,17 @@ int init (void)
     return (SLURM_SUCCESS);
 }
 
+int prep_p_prolog(job_env_t *job_env, slurm_cred_t *cred)
+{
+    printf("%s\n", "ProLog");
+    return (SLURM_SUCCESS);
+}
+
+int prep_p_epilog(job_env_t *job_env, slurm_cred_t *cred)
+{
+    printf("%s\n", "Epilog");
+    return (SLURM_SUCCESS);
+}
 
 void fini (void)
 {
