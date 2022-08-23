b4d0bddba25bf02aa009daab15f4963e8cd214f5
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-08-23 12:00:53 +0200

    Simplified get logtime function.

--- src/gatherers/logs/get_log_time.c
+++ src/gatherers/logs/get_log_time.c
@@ -13,25 +13,19 @@ void get_log_time(parsed_log_t *log_to_parse)
     FILE *uptime_file = NULL;
     char *uptime_str = NULL;
     time_t uptime_sec = 0;
-    time_t curr_time = 0;
-    time_t boot_time = 0;
     time_t log_time = 0;
     size_t uptime_str_len = 30;
 
-    if (log_to_parse->log_source_path == NULL)
+    if (log_to_parse->log_source_path == NULL || strcmp(log_to_parse->log_source_path, "syslog") != 0)
         return;
-    if (strcmp(log_to_parse->log_source_path, "syslog") == 0) {
-        uptime_file = fopen("/proc/uptime", "r");
-        if (uptime_file == NULL)
-            return;
-        getline(&uptime_str, &uptime_str_len, uptime_file);
-        uptime_sec = (time_t)atol(uptime_str);
-        fclose(uptime_file);
-    }
+    uptime_file = fopen("/proc/uptime", "r");
+    if (uptime_file == NULL)
+        return;
+    getline(&uptime_str, &uptime_str_len, uptime_file);
+    uptime_sec = (time_t)atol(uptime_str);
+    fclose(uptime_file);
     log_time = (time_t)atol((log_to_parse->unparsed_log + 1));
-    curr_time = time(NULL);
-    boot_time = curr_time - uptime_sec;
-    log_time += boot_time;
+    log_time += (time(NULL) - uptime_sec); //boot time + time since boot to log
     log_to_parse->log_time_str = strdup(asctime(localtime(&log_time)));
     free(uptime_str);
 }
