bb012e85536d44ca0d3d2f89d0540cad9acd4661
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-08-24 14:14:18 +0200

    Made a subfunction to gather kernel logs (landle log time) for better practice and readability.

--- src/gatherers/logs/gather_kernel_logs.c
+++ src/gatherers/logs/gather_kernel_logs.c
@@ -9,6 +9,19 @@
 #include "slurm/slurm.h"
 #include "demeter.h"
 
+static bool handle_log_time(job_id_info_t *job_info, parsed_log_t *curr_log, demeter_conf_t *demeter_conf)
+{
+    if (get_log_time(curr_log, job_info->start_time) != 0) {
+        write_log_to_file(demeter_conf, "Cannot get log time/log is not written at runtime", DEBUG, 99);
+        if (curr_log->unparsed_log != NULL)
+            free(curr_log->unparsed_log);
+        if (curr_log->log_source_path != NULL)
+            free(curr_log->log_source_path);
+        return false;
+    }
+    return true;
+}
+
 linked_list_t *gather_kernel_logs
 (demeter_conf_t *demeter_conf, job_id_info_t *job_info,
 cgroup_data_t *cgroup_data, linked_list_t *log_list)
@@ -17,8 +30,7 @@ cgroup_data_t *cgroup_data, linked_list_t *log_list)
     parsed_log_t *curr_log = NULL;
     static int len = 0;
 
-    log_buffer = read_sys_logs();
-    if (log_buffer == NULL)
+    if ((log_buffer = read_sys_logs()) == NULL)
         return (NULL);  
     log_list = add_to_list(log_list, init_parsed_log());
     for (int i = 0; log_buffer[i] != '\0'; i++) {
@@ -30,23 +42,14 @@ cgroup_data_t *cgroup_data, linked_list_t *log_list)
             continue;
         curr_log->unparsed_log = strndup(log_buffer + i, len);
         i += len - 1;
-        if (curr_log->unparsed_log == NULL || is_log_empty(curr_log->unparsed_log)) {
-            write_log_to_file(demeter_conf, "Caught empty log line", DEBUG, 2);
+        if (curr_log->unparsed_log == NULL || is_log_empty(curr_log->unparsed_log))
             continue;
-        }
         curr_log->log_source_path = strdup("syslog");
-        if (get_log_time(curr_log, job_info->start_time) != 0) {
-            write_log_to_file(demeter_conf, "Cannot get log time/log is not written at runtime", DEBUG, 99);
-            if (curr_log->unparsed_log != NULL)
-                free(curr_log->unparsed_log);
-            if (curr_log->log_source_path != NULL)
-                free(curr_log->log_source_path);
+        if (!handle_log_time(job_info, curr_log, demeter_conf))
             continue;
-        }
         curr_log->log_proc_name = strdup("kernel");
         log_list = add_to_list(log_list, init_parsed_log());
     }
-    if (log_buffer != NULL)
-        free(log_buffer);
+    free(log_buffer);
     return (log_list);
 }
